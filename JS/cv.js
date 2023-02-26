51ab956a22ff3e55c4a8bf78670052ab373402e8
Facundo Contreras <facu_nc@hotmail.com>
2023-02-26 13:19:49 -0300

    carga de datos con json finalizado

---
+++ JS/cv.js
@@ -0,0 +1,30 @@
+//Cargar datos de perfil de Randomuser.me
+async function consulta(){
+    let datosPersonales = await fetch("https://randomuser.me/api/")
+        .then(response => response.json())
+        .then(data => data)
+    dato = datosPersonales.results[0];
+    var foto = document.getElementById('foto');
+
+    foto.setAttribute('src', dato.picture.large);
+    document.getElementById('nombre').innerHTML = dato.name.first + " " + dato.name.last;
+    document.getElementById('origen').innerHTML = dato.nat;
+    document.getElementById('edad').innerHTML = dato.dob.age;
+    document.getElementById('residencia').innerHTML = dato.location.street.number +' '+ dato.location.street.name + ', ' + dato.location.city + ', ' + dato.location.country + ' ' + '(' + dato.location.postcode + ')'; 
+}
+
+consulta();
+
+
+//Validacion del formulario
+
+function validateForm() {
+    let nombre = document.forms["formulario"]['nombre'].value;
+    let email = document.forms["formulario"]['email'].value;
+
+    if(nombre == '' || email == ''){
+        alert('"Nombre" y "Correo electronico" son campos obligatorios');
+    }
+} 
+
+document.getElementById("enviar").addEventListener("click", validateForm);
