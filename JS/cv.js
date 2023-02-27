c9db0b5a4eff20607cf4e6e9cf8fcf4cb7e1e850
Facundo Contreras <facu_nc@hotmail.com>
2023-02-27 19:04:16 -0300

    font-style finalizado

--- JS/cv.js
+++ JS/cv.js
@@ -1,16 +1,19 @@
 //Cargar datos de perfil de Randomuser.me
+
 async function consulta(){
-    let datosPersonales = await fetch("https://randomuser.me/api/")
+    let datosPersonales = await fetch("https://randomuser.me/api/?gender=male")
         .then(response => response.json())
         .then(data => data)
+
     dato = datosPersonales.results[0];
     var foto = document.getElementById('foto');
 
     foto.setAttribute('src', dato.picture.large);
-    document.getElementById('nombre').innerHTML = dato.name.first + " " + dato.name.last;
+    document.getElementById('nombre').innerHTML = dato.name.first;
+    document.getElementById('nombre-completo').innerHTML = dato.name.first + " " + dato.name.last;
     document.getElementById('origen').innerHTML = dato.nat;
     document.getElementById('edad').innerHTML = dato.dob.age;
-    document.getElementById('residencia').innerHTML = dato.location.street.number +' '+ dato.location.street.name + ', ' + dato.location.city + ', ' + dato.location.country + ' ' + '(' + dato.location.postcode + ')'; 
+    document.getElementById('residencia').innerHTML = dato.location.street.name + ' ' + dato.location.street.number + ', ' + dato.location.city + ', ' + dato.location.country + ' ' + '(' + dato.location.postcode + ')'; 
 }
 
 consulta();
@@ -24,6 +27,9 @@ function validateForm() {
 
     if(nombre == '' || email == ''){
         alert('"Nombre" y "Correo electronico" son campos obligatorios');
+    }
+    else{
+        alert('Su mensaje a sido enviado con exito. Me contactare a la brevedad. Muchas gracias!!!')
     }
 } 
 
