461a7715d99fc5f427000832bb3cddcebc0b137e
facunc <122060147+facunc@users.noreply.github.com>
2023-03-08 15:05:14 -0300

    Add files via upload
    menu desplegable, flecha arriba y JS de form finalizados

---
+++ JS/cv.js
@@ -0,0 +1,44 @@
+
+//Cargar datos de perfil de Randomuser.me con fetch()
+
+async function consulta(){
+    let datosPersonales = await fetch("https://randomuser.me/api/?gender=male")
+        .then(response => response.json())
+        .then(data => data)
+
+    dato = datosPersonales.results[0];
+    var foto = document.getElementById('foto');
+
+    foto.setAttribute('src', dato.picture.large);
+    document.getElementById('nombre').innerHTML = dato.name.first;
+    document.getElementById('nombre-completo').innerHTML = dato.name.first + " " + dato.name.last;
+    document.getElementById('origen').innerHTML = dato.nat;
+    document.getElementById('edad').innerHTML = dato.dob.age;
+    document.getElementById('residencia').innerHTML = dato.location.street.name + ' ' + dato.location.street.number + ', ' + dato.location.city + ', ' + dato.location.country + ' ' + '(' + dato.location.postcode + ')'; 
+}
+
+consulta();
+
+
+// Mensaje de formulario enviado con éxito
+
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener("submit", () => {
+    alert('Gracias! Su mensaje fue enviado con éxito. Me contactaré a la brevedad');
+});
+
+
+
+// Animación de la flecha ir arriba
+
+window.onscroll = function (){irArriba()}
+function irArriba(){
+    
+    if(document.documentElement.scrollTop > 50){
+        document.getElementById('flecha').className = 'container-flecha-arriba';
+    }
+    else{
+        document.getElementById('flecha').className = '';
+    }
+}
